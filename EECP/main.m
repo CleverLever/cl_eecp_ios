f4a2d0695eb465f7462e0ab06f232552082569cc
Austin Gregg <austin.gregg@fellowshipchurch.com>
2013-10-14 17:45:55 -0700

    Initial Commit

---
+++ EECP/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  EECP
+//
+//  Created by Austin Gregg on 10/14/13.
+//  Copyright (c) 2013 CleverLever. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "CLAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([CLAppDelegate class]));
+    }
+}
